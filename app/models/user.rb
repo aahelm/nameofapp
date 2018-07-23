ae9907694a9777277624469ec0445a0eda13f7f0
aahelm <aaron.c.helm@gmail.com>
2018-07-22 22:15:18 -0400

    5.7 comment models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User <  ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :orders
-
+  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
