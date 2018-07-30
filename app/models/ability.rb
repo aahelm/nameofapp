5033dd0545772819e1a00c615f4a34bc7d9adc11
aahelm <aaron.c.helm@gmail.com>
2018-07-29 22:09:22 -0400

    5.10 added admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    can :read, :all .
+    if user.present?
+      can :manage, Post, user_id: user.id
+      if user.admin?
+        can :manage, :all
+      end
+    end
   end
 end
