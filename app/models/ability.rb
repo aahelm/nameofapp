1690bbeb1400f3bffb0af53cbd2efd4ed780224a
aahelm <aaron.c.helm@gmail.com>
2018-07-29 23:24:08 -0400

    more fixes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,13 +1,13 @@
 class Ability
   include CanCan::Ability
+end
 
   def initialize(user)
     can :read, :all .
-    if user.signed_in?
+    if user.present?
       can :manage, Post, user_id: user.id
       if user.admin?
         can :manage, :all
       end
     end
   end
-end
