4a08bbf12793a7b1f44d7b318bf16051dafafeed
aahelm <aaron.c.helm@gmail.com>
2018-07-29 23:09:12 -0400

    5.10 fixes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     can :read, :all .
-    if user.present?
+    if user.signed_in?
       can :manage, Post, user_id: user.id
       if user.admin?
         can :manage, :all
