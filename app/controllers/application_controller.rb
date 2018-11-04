b8fc25141adb0992ea8ecb968e11bd6aee398be0
aahelm <aaron.c.helm@gmail.com>
2018-11-04 15:03:46 -0500

    6.14 changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
   redirect_to main_app.root_url, alert: exception.message
 end
+
+before_action :set_products
+def set_products
+    @products = Product.all
+end
+
 end
